ed4967d2b899f3c39756ec0b5b3f5ff41e8dccbb
codArtic0 <raulsousadesigner@gmail.com>
2026-04-08 14:19:39 -0300

    FIX: Catch de erros de consulta TACO

--- controllers/tacoController.js
+++ controllers/tacoController.js
@@ -1,30 +1,40 @@
 const prisma = require("../lib/prisma")
 
 exports.getAlimentoByName = async (req, res) => {
-
-    const name = req.query.name
-    const alimento = await prisma.alimento.findFirst({
-        where: {
-            nome:{
-                contains: name,
-                mode: 'insensitive'
+    try {
+        const name = req.query.name
+        const alimento = await prisma.alimento.findFirst({
+            where: {
+                nome:{
+                    contains: name,
+                    mode: 'insensitive'
+                }
             }
-        }
-    })
+        })
 
-    return res.status(200).json(alimento)
+        return res.status(200).json(alimento)
+    } catch (error) {
+        return res.status(500).json({error: "Erro interno do servidor"})
+    }
+
+    
 }
 
 exports.getManyAlimentos = async (req, res) =>{
-    const name = req.query.name
-    const alimentos = await prisma.alimento.findMany({
-        where:{
-            nome:{
-                contains: name,
-                mode: 'insensitive'
+    try {
+        const name = req.query.name
+        const alimentos = await prisma.alimento.findMany({
+            where:{
+                nome:{
+                    contains: name,
+                    mode: 'insensitive'
+                }
             }
-        }
-    })
+        })
 
-    return res.status(200).json(alimentos)
+        return res.status(200).json(alimentos)
+    } catch (error) {
+        return res.status(500).json({error: "Erro interno do servidor"})
+    }
+    
 }
