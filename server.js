9e3c6292a7b58cbfcb53467f625e1078f03c9c0e
codArtic0 <raulsousadesigner@gmail.com>
2026-05-06 11:47:06 -0300

    FIX: Port Config

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require('./app')
-const { PORT } = require('./config/env')
+const { PORT: configPort } = require('./config/env')
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || configPort || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`)
-  console.log(`DOCUMENTAÇÃO DISPONÍVEL EM: http://localhost:${PORT}/api-docs/`)
-  console.log("NOTA: Essa API NÃO SUBSTITUI uma availiação nutricional. São apenas valores estimados baseados em fórmulas matemáticas!")
+  console.log(`Documentação: https://api-fit-j5og.onrender.com/api-docs/`)
+  console.log("NOTA: Essa API não substitui uma avaliação nutricional.")
 })
